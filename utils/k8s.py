a4ee05d3093c2276b402131098dc82776d26ffba
Olly-n <ollycjnorman@googlemail.com>
2025-07-31 19:23:29 +0100

    Add k8s deletion update function

---
+++ utils/k8s.py
@@ -0,0 +1,48 @@
+import os
+
+import requests
+
+import utils.logging as log
+
+
+def update_pod_deletion_cost(deletion_cost: int) -> bool:
+    pod_name = os.getenv("POD_NAME")
+    k8s_service_host = os.getenv("K8S_SERVICE_HOST")
+    k8s_service_port = os.getenv("k8s_service_port")
+    if not pod_name:
+        log.log(
+            "This worker does not seem to be running in K8s pod and therefore can not update the deletion cost."
+        )
+        return False
+
+    patch = [
+        {
+            "op": "replace",
+            "path": "/metadata/annotations",
+            "value": {"controller.kubernetes.io/pod-deletion-cost": str(deletion_cost)},
+        }
+    ]
+    try:
+        with open(
+            "/var/run/secrets/kubernetes.io/serviceaccount/token", "r", encoding="utf8"
+        ) as file:
+            k8s_token = file.read()
+    except FileNotFoundError:
+        log.log("Kubernetes token file not found.")
+        return False
+
+    headers = {
+        "Content-type": "application/json-patch+json",
+        "Authorization": f"Bearer {k8s_token}",
+    }
+
+    patch_url = f"https://{k8s_service_host}:{k8s_service_port}/api/v1/namespaces/default/pods/{pod_name}"
+
+    try:
+        response = requests.patch(url=patch_url, headers=headers, json=patch)
+        if response.status != 200:
+            raise Exception(f"k8s patch failed: {response.status}")
+        return True
+    except Exception as e:
+        log.log(str(e))
+        return False
